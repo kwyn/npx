ea65260627c25e812f8249775e71918b3da7cc56
HarleyKwyn <kwyn.meagher@gmail.com>
2014-11-15 20:38:42 -0800

    Refactored to us lodash

--- examples/npx-example.js
+++ examples/npx-example.js
@@ -1,12 +1,12 @@
 var Npx = require('../'); // Replace '../' with 'npx' in your code
-
+var _ = require('lodash');
 var npx = new Npx(60,1);
 // npx.setAll([0x00,0x11,0x11])()
 //   // .then( npx.clear() )
 //   .then(function(){ console.log('did the thing') });
 var white = [0xFF,0xFF,0xFF];
 var off = '#000000';
-var red = '';
+var red = [0x22,0x00,0x00];
 var orange = [0x33, 0x11, 0x00];
 var yellow = [0x11, 0x11, 0x00];
 var green = [0x00, 0x11,  0x00];
@@ -22,30 +22,34 @@ var colors = [red, orange, yellow, green, blue, violet];
 
 var walk = function (colorArray){
   // For every pixel we want a new frame;
+  console.log(colorArray)
   var animation = npx.newAnimation(60, 60);
-  var frame;
-  for (var i = 0; i < animation.length; i++) {
-    // now write to the spots in the frame that you want colored
-    // in this case I just want it to walk along one by one
-    for (var j = 0; j < colorArray.length; j++) {
-      // Remember now, I is the frame we're in so we'll increment out starting point by the frame that we're on.
-      // j is the specific color we're focusing on and k for every byte in that color.
-      var pixelIndex = i+j < animation.framelength ? i+j: (i+j - animation.framelength);
-      console.log(pixelIndex, colorArray[j], i);
-      animation.setPixel(pixelIndex, colorArray[j], i);
-    }
-  }
+  _.each( _.range(animation.length), function(frame){
+    _.each(colorArray, function(color, colorIndex){
+      var pixelIndex = frame + colorIndex;
+      if(frame + colorIndex <= animation.framelength ){
+        pixelIndex = pixelIndex - animation.framelength;
+      }
+      animation.setPixel(pixelIndex, color, frame);
+    });
+  });
   return animation;
 }
 
-// var walkAnimation = walk(colors);
-// for (var i = 0; i < 10; i++) {
-//   npx.enqueue(walkAnimation, 1000);
-// }
-
-var animation = new npx.newAnimation(60,1);
-animation.setAll("#0F0600")
-npx.play(animation)
+var walkAnimation = walk(colors);
+for (var i = 0; i < 10; i++) {
+  npx.enqueue(walkAnimation, 1000);
+}
+npx.run();
+// npx.loop(walkAnimation);
+// setTimeout(function(){
+//   npx.stop();
+// },5000)
+// var animation = new npx.newAnimation(60,1);
+// animation.setPattern(colors);
+// var animation = new npx.newAnimation(60,1);
+// animation.setAll("#0F0600")
+// npx.play(animation)
 // npx.run()
 //   .then(function(){console.log('done')});
 
