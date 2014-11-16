48ac253d6e95bec506795c9df4e30b48d5c82aaf
HarleyKwyn <kwyn.meagher@gmail.com>
2014-11-15 22:33:03 -0800

    impliment set pattern function :D

--- examples/npx-example.js
+++ examples/npx-example.js
@@ -1,17 +1,17 @@
 var Npx = require('../'); // Replace '../' with 'npx' in your code
 var _ = require('lodash');
-var npx = new Npx(60,1);
+var npx = new Npx(60);
 // npx.setAll([0x00,0x11,0x11])()
 //   // .then( npx.clear() )
 //   .then(function(){ console.log('did the thing') });
 var white = [0xFF,0xFF,0xFF];
 var off = '#000000';
-var red = [0xFF,0x00,0x00];
-var orange = [0x33, 0x11, 0x00];
-var yellow = [0x11, 0x11, 0x00];
-var green = [0x00, 0x11,  0x00];
-var blue = [0x00, 0x00, 0x11];
-var violet = [0x11, 0x00, 0x11];
+var red = [0x0f,0x00,0x00];
+var orange = "#0F0600";
+var yellow = [0x0f, 0x0f, 0x00];
+var green = [0x00, 0x0f,  0x00];
+var blue = [0x00, 0x00, 0x0f];
+var violet = [0x11, 0x00, 0x09];
 var colors = [red, orange, yellow, green, blue, violet];
 
 
@@ -23,11 +23,11 @@ var colors = [red, orange, yellow, green, blue, violet];
 var walk = function (colorArray){
   // For every pixel we want a new frame;
   console.log(colorArray)
-  var animation = npx.newAnimation(60, 60);
-  _.each( _.range(animation.length), function(frame){
+  var animation = npx.newAnimation(60, 1);
+  _.each(animation.frames, function(frame){
     _.each(colorArray, function(color, colorIndex){
       var pixelIndex = frame + colorIndex;
-      if(frame + colorIndex >= animation.framelength){
+      if(frame + colorIndex >= animation.framelength-1){
         pixelIndex = pixelIndex - animation.framelength;
       }
       animation.setPixel(pixelIndex, color, frame);
@@ -36,17 +36,21 @@ var walk = function (colorArray){
   return animation;
 }
 
-var walkAnimation = walk(colors);
-for (var i = 0; i < 10; i++) {
-  npx.enqueue(walkAnimation, 1000);
-}
-npx.run();
+// var walkAnimation = walk(colors);
+// for (var i = 0; i < 10; i++) {
+//   npx.enqueue(walkAnimation, 1000);
+// }
+// npx.run();
 // npx.loop(walkAnimation);
 // setTimeout(function(){
 //   npx.stop();
 // },5000)
-// var animation = new npx.newAnimation(60,1);
-// animation.setPattern(colors);
+
+var animation = npx.newAnimation(1,1);
+animation.setPattern(colors);
+npx.play(animation);
+
+
 // var animation = new npx.newAnimation(60,1);
 // animation.setAll("#0F0600")
 // npx.play(animation)
