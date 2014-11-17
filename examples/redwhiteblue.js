277a0e46d4fd45aafcbc469e39205d485d71d2ec
HarleyKwyn <kwyn.meagher@gmail.com>
2014-11-16 22:14:32 -0800

    Add red white and blue examples

---
+++ examples/redwhiteblue.js
@@ -0,0 +1,19 @@
+// Rainbow animation
+
+// Require Npx module.
+var Npx = require('../'); // Replace '../' with 'npx';
+
+// Initialize npx object with number of neopixels
+var npx = new Npx(60);
+
+// Create colored animations
+var animationRed = npx.newAnimation(1).setAll('#FF0000');
+var animationWhite = npx.newAnimation(1).setAll('#FFFFFF');
+var animationBlue = npx.newAnimation(1).setAll('#0000FF');
+
+//queue up animations
+npx.enqueue(animationRed, 1000)
+   .enqueue(animationWhite, 1000)
+   .enqueue(animationBlue, 1000)
+   //run animations :D
+   .run();
